f03dd87f2c0b727d5a2551a174fcb79c61691899
Alexander Krutikov <alexkrutik0ff@gmail.com>
2022-03-02 00:39:35 +0300

    part19-20

--- src/views/History.vue
+++ src/views/History.vue
@@ -1,158 +1,163 @@
 <template>
-	<div>
-		<div class="page-title">
-			<h3>История записей</h3>
-		</div>
+  <div>
+    <div class="page-title">
+      <h3>История записей</h3>
+    </div>
 
-		<Loader v-if="loading" />
+    <Loader v-if="loading" />
 
-		<p class="center" v-else-if="!records.length">
-			Записей пока нет
-			<router-link to="/record">Добавьте первую</router-link>
-		</p>
+    <p class="center" v-else-if="!records.length">
+      Записей пока нет
+      <router-link to="/record">Добавьте первую</router-link>
+    </p>
 
-		<section v-else>
-			<div class="row">
-				<div class="history-chart col s6">
-					<pie-out-chart
-						:chartdata="chartOutcomeData"
-						:options="chartOptions"
-					/>
-				</div>
-				<div class="history-chart col s6">
-					<pie-in-chart :chartdata="chartIncomeData" :options="chartOptions" />
-				</div>
-			</div>
+    <section v-else>
+      <div class="row">
+        <div class="history-chart col m12 l6">
+          <pie-out-chart
+            :chartdata="chartOutcomeData"
+            :options="chartOptions"
+          />
+        </div>
+        <div class="history-chart col m12 l6">
+          <pie-in-chart :chartdata="chartIncomeData" :options="chartOptions" />
+        </div>
+      </div>
 
-			<HistoryTable :records="items" />
-			<Paginate
-				v-model="page"
-				:page-count="pageCount"
-				:click-handler="pageChangeHandler"
-				:prev-text="'Назад'"
-				:next-text="'Вперед'"
-				:container-class="'pagination'"
-				:page-class="'waves-effect'"
-			/>
-		</section>
-	</div>
+      <HistoryTable :records="items" />
+      <Paginate
+        v-model="page"
+        :page-count="pageCount"
+        :click-handler="pageChangeHandler"
+        :prev-text="'Назад'"
+        :next-text="'Вперед'"
+        :container-class="'pagination'"
+        :page-class="'waves-effect'"
+      />
+    </section>
+  </div>
 </template>
 
 <script>
-import paginationMixin from "@/mixins/pagination.mixin"
-import HistoryTable from "@/components/HistoryTable"
-import Loader from "@/components/app/Loader.vue"
-import PieInChart from "@/components/Chart.js"
-import PieOutChart from "@/components/Chart.js"
+import paginationMixin from '@/mixins/pagination.mixin'
+import HistoryTable from '@/components/HistoryTable'
+import Loader from '@/components/app/Loader.vue'
+import PieInChart from '@/components/Chart.js'
+import PieOutChart from '@/components/Chart.js'
 
 export default {
-	name: "history",
-	mixins: [paginationMixin],
-	data: () => ({
-		loading: true,
-		records: [],
-		chartOutcomeData: null,
-		chartIncomeData: null,
-		chartOptions: {
-			responsive: true,
-			maintainAspectRatio: false,
-		},
-	}),
-	async mounted() {
-		this.records = await this.$store.dispatch("fetchRecords")
-		const categories = await this.$store.dispatch("fetchCategories")
+  name: 'history',
+  metaInfo() {
+    return {
+      title: this.$title('Menu_History'),
+    }
+  },
+  mixins: [paginationMixin],
+  data: () => ({
+    loading: true,
+    records: [],
+    chartOutcomeData: null,
+    chartIncomeData: null,
+    chartOptions: {
+      responsive: true,
+      maintainAspectRatio: false,
+    },
+  }),
+  async mounted() {
+    this.records = await this.$store.dispatch('fetchRecords')
+    const categories = await this.$store.dispatch('fetchCategories')
 
-		this.setup(categories)
+    this.setup(categories)
 
-		this.loading = false
-	},
-	methods: {
-		setup(categories) {
-			this.setupPagination(
-				this.records.map((record) => {
-					return {
-						...record,
-						categoryName: categories.find((c) => c.id === record.categoryId)
-							.title,
-						typeClass: record.type === "income" ? "green" : "red",
-						typeText: record.type === "income" ? "Доход" : "Расход",
-					}
-				}),
-			)
+    this.loading = false
+  },
+  methods: {
+    setup(categories) {
+      this.setupPagination(
+        this.records.map((record) => {
+          return {
+            ...record,
+            categoryName: categories.find((c) => c.id === record.categoryId)
+              .title,
+            typeClass: record.type === 'income' ? 'green' : 'red',
+            typeText: record.type === 'income' ? 'Доход' : 'Расход',
+          }
+        })
+      )
 
-			this.chartOutcomeData = {
-				labels: categories.map((c) => c.title),
-				datasets: [
-					{
-						label: "Расходы по категориям",
-						data: categories.map((c) => {
-							return this.records.reduce((total, r) => {
-								if (r.categoryId === c.id && r.type === "outcome") {
-									total += +r.amount
-								}
-								return total
-							}, 0)
-						}),
-						backgroundColor: [
-							"rgba(255, 99, 132, 0.2)",
-							"rgba(54, 162, 235, 0.2)",
-							"rgba(255, 206, 86, 0.2)",
-							"rgba(75, 192, 192, 0.2)",
-							"rgba(153, 102, 255, 0.2)",
-							"rgba(255, 159, 64, 0.2)",
-						],
-						borderColor: [
-							"rgba(255, 99, 132, 1)",
-							"rgba(54, 162, 235, 1)",
-							"rgba(255, 206, 86, 1)",
-							"rgba(75, 192, 192, 1)",
-							"rgba(153, 102, 255, 1)",
-							"rgba(255, 159, 64, 1)",
-						],
-						borderWidth: 1,
-					},
-				],
-			}
+      this.chartOutcomeData = {
+        labels: categories.map((c) => c.title),
+        datasets: [
+          {
+            label: 'Расходы по категориям',
+            data: categories.map((c) => {
+              return this.records.reduce((total, r) => {
+                if (r.categoryId === c.id && r.type === 'outcome') {
+                  total += +r.amount
+                }
+                return total
+              }, 0)
+            }),
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 1,
+          },
+        ],
+      }
 
-			this.chartIncomeData = {
-				labels: categories.map((c) => c.title),
-				datasets: [
-					{
-						label: "Доходы по категориям",
-						data: categories.map((c) => {
-							return this.records.reduce((total, r) => {
-								if (r.categoryId === c.id && r.type === "income") {
-									total += +r.amount
-								}
-								return total
-							}, 0)
-						}),
-						backgroundColor: [
-							"rgba(255, 99, 132, 0.2)",
-							"rgba(54, 162, 235, 0.2)",
-							"rgba(255, 206, 86, 0.2)",
-							"rgba(75, 192, 192, 0.2)",
-							"rgba(153, 102, 255, 0.2)",
-							"rgba(255, 159, 64, 0.2)",
-						],
-						borderColor: [
-							"rgba(255, 99, 132, 1)",
-							"rgba(54, 162, 235, 1)",
-							"rgba(255, 206, 86, 1)",
-							"rgba(75, 192, 192, 1)",
-							"rgba(153, 102, 255, 1)",
-							"rgba(255, 159, 64, 1)",
-						],
-						borderWidth: 1,
-					},
-				],
-			}
-		},
-	},
-	components: {
-		HistoryTable,
-		PieInChart,
-		PieOutChart,
-	},
+      this.chartIncomeData = {
+        labels: categories.map((c) => c.title),
+        datasets: [
+          {
+            label: 'Доходы по категориям',
+            data: categories.map((c) => {
+              return this.records.reduce((total, r) => {
+                if (r.categoryId === c.id && r.type === 'income') {
+                  total += +r.amount
+                }
+                return total
+              }, 0)
+            }),
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 1,
+          },
+        ],
+      }
+    },
+  },
+  components: {
+    HistoryTable,
+    PieInChart,
+    PieOutChart,
+  },
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,48 +1,53 @@
 <template>
-	<div>
-		<div class="page-title">
-			<h3>Счет</h3>
+  <div>
+    <div class="page-title">
+      <h3>Счет</h3>
 
-			<button class="btn waves-effect waves-light btn-small" @click="refresh">
-				<i class="material-icons">refresh</i>
-			</button>
-		</div>
+      <button class="btn waves-effect waves-light btn-small" @click="refresh">
+        <i class="material-icons">refresh</i>
+      </button>
+    </div>
 
-		<Loader v-if="loading" />
+    <Loader v-if="loading" />
 
-		<div v-else class="row">
-			<HomeBill :rates="currency.rates" />
-			<HomeCurrency :rates="currency.rates" :date="currency.date" />
-		</div>
-	</div>
+    <div v-else class="row">
+      <HomeBill :rates="currency.rates" />
+      <HomeCurrency :rates="currency.rates" :date="currency.date" />
+    </div>
+  </div>
 </template>
 
 <script>
-import HomeBill from "@/components/HomeBill"
-import HomeCurrency from "@/components/HomeCurrency"
-import Loader from "@/components/app/Loader"
+import HomeBill from '@/components/HomeBill'
+import HomeCurrency from '@/components/HomeCurrency'
+import Loader from '@/components/app/Loader'
 
 export default {
-	name: "Home",
-	data: () => ({
-		loading: true,
-		currency: null,
-	}),
-	async mounted() {
-		this.loading = true
-		this.currency = await this.$store.dispatch("fetchCurrency")
-		this.loading = false
-	},
-	methods: {
-		async refresh() {
-			this.loading = true
-			this.currency = await this.$store.dispatch("fetchCurrency")
-			this.loading = false
-		},
-	},
-	components: {
-		HomeBill,
-		HomeCurrency,
-	},
+  name: 'Home',
+  metaInfo() {
+    return {
+      title: this.$title('Menu_Bill'),
+    }
+  },
+  data: () => ({
+    loading: true,
+    currency: null,
+  }),
+  async mounted() {
+    this.loading = true
+    this.currency = await this.$store.dispatch('fetchCurrency')
+    this.loading = false
+  },
+  methods: {
+    async refresh() {
+      this.loading = true
+      this.currency = await this.$store.dispatch('fetchCurrency')
+      this.loading = false
+    },
+  },
+  components: {
+    HomeBill,
+    HomeCurrency,
+  },
 }
 </script>
