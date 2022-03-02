7494f6978fd1e02f0ef4e019773fa25f7f077407
Alexander Krutikov <alexkrutik0ff@gmail.com>
2022-03-02 22:56:06 +0300

    finish

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ import titlePlugin from '@/utils/title.plugin'
 import Loader from '@/components/app/Loader'
 import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 import { initializeApp } from 'firebase/app';
 import { getAuth, onAuthStateChanged } from "firebase/auth";
